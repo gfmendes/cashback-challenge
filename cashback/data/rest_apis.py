6837cd78efc22e5303c62aee448d30d4903620d4
gfmendes <gmendes@gmail.com>
2020-06-19 11:00:22 -0300

    simplifing return statment in CashBackAPI

--- cashback/data/rest_apis.py
+++ cashback/data/rest_apis.py
@@ -1,11 +1,11 @@
-import requests, ast
+import requests
 
 class CashBackAPI():
   def get_cashback_amount(self, cpf):
-    url="https://mdaqk8ek5j.execute-api.us-east-1.amazonaws.com/v1/cashback?cpf=12312312323"
+    url="https://mdaqk8ek5j.execute-api.us-east-1.amazonaws.com/v1/cashback?cpf={}".format(cpf)
     head = {'Content-type':'application/json', 'token':'ZXPURQOARHiMc6Y0flhRC1LVlZQVFRnm'}
     response = requests.get(url, headers=head)
-    return ast.literal_eval(response.text)['body']
+    return response.json()['body']
 
 
     
